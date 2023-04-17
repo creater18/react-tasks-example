1a82aa9c30dc6a19cd0648446c6b59a4748cfc5a
creater18 <creatorarmendariz@hotmail.com>
2023-04-17 06:56:09 -0600

    firs commit

---
+++ src/data/tasks.js
@@ -0,0 +1,19 @@
+// como no es una interfaz o componente sse puede llamaar .js
+
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "mi tercer tarea",
+    description: "mi tercer tarea",
+  }
+];
